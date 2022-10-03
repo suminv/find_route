4282eb7b9007db4691f62f05f12948a544d847f7
Vlad <vlads1976@gmail.com>
2022-10-03 16:11:13 +0200

    Added customization error pages.

--- travel/settings.py
+++ travel/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3tkztwxodg-4n5ndy2mz0mhbr5aivo62yo5^d^^i9tx0-hy(qn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
